455b2fce8af8e0af91a2ad1a939bd0c0680c7c0d
liuli <liuliminamy@gmail.com>
2019-11-12 12:17:47 -0500

    Finished lab 6

---
+++ app/src/main/java/com/example/fall_1st/WeatherForecastActivity.java
@@ -0,0 +1,13 @@
+package com.example.fall_1st;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+    }
+}
